60ca206055e150e53e9581df37301d1f68088e84
Madhan S <madswork007@gmail.com>
2025-12-30 10:16:59 +0530

    Create a Queue Implementation using Linked list in C

---
+++ Queue Implementation/Queue Implementation using Linked list.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *front=NULL;
+struct node *rear=NULL;
+int main()
+{
+    int n,value;
+    printf("Enter no.of elements to enqueue: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        struct node *newnode=malloc(sizeof(struct node));
+        scanf("%d",&value);
+        
+        newnode->data=value;
+        newnode->next=NULL;
+        
+        if (rear==NULL){
+            front=rear=newnode;
+        }
+        else{
+            rear->next=newnode;
+            rear=newnode;
+        }
+    }
+    printf("\nQueue using Linked list: ");
+    struct node *temp=front;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    return 0;
+}
