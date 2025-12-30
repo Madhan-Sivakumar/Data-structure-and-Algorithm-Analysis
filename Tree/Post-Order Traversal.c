34378c3ffa65fdc0e95c5e059e021191c6adc409
Madhan S <madswork007@gmail.com>
2025-12-30 11:41:04 +0530

    Create a Post-Order Traversal in Tree using C

---
+++ Tree/Post-Order Traversal.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createnode(int value){
+    struct node *newnode = malloc(sizeof(struct node));
+    newnode->data = value;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
+
+struct node *createtree(){
+    int value;
+    printf("ENTER NODE VALUE(-1 FOR NO NODE): ");
+    scanf("%d",&value);
+    if(value == -1){
+        return NULL;
+    }
+    struct node *root = createnode(value);
+    printf("ENTER LEFT CHILD OF %d\n", value);
+    root->left = createtree();
+    printf("ENTER RIGHT CHILD OF %d\n", value);
+    root->right = createtree();
+    return root;
+}
+
+void postorder(struct node *root){
+    if(root != NULL){
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main(){
+    struct node *root;
+    printf("CREATE BINARY TREE\n");
+    root = createtree();
+    printf("\nPOSTORDER TRAVERSAL: ");
+    postorder(root);
+    return 0;
+}
